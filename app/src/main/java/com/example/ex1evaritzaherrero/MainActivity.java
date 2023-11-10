48e5bc58190813f7de76df56078683e72c5a7b67
Aritza <aritz.herreroni@elorrieta-errekamari.com>
2023-11-10 11:40:23 +0100

    Initial commit

---
+++ app/src/main/java/com/example/ex1evaritzaherrero/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ex1evaritzaherrero;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
